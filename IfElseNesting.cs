6d34a27db8bdec6ca657132f49323afc58727b98
Umaidkhan12 <110336799+Umaidkhan12@users.noreply.github.com>
2024-07-09 00:14:37 +0530

    Add files via upload

---
+++ IfElseNesting.cs
@@ -0,0 +1,30 @@
+class IfElseNesting
+{
+	public static void Main(string[] args)
+	{
+		int a = 325, b = 712, c = 478;
+		System.Console.Write("Largest Value is : ");
+		if(a > b)
+		{
+			if(a > c)
+			{
+				System.Console.WriteLine(a);
+			}
+			else
+			{
+				System.Console.WriteLine(c);
+			}
+		}
+		else
+		{
+			if(c > b)
+			{
+				System.Console.WriteLine(c);
+			}
+			else
+			{
+				System.Console.WriteLine(b);
+			}
+		}
+	}
+}
